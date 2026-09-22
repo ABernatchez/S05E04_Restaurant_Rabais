9d19dbadd56e233ea8d086b58f30fd5a330111aa
abernatchez <abernatchez@tutamail.com>
2026-09-22 18:55:39 -0400

    Ajoute stratégie de rabais appliquant multiples rabais

---
+++ S05E04_Restaurant_Rabais.Tests/RabaisMultipleTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant.Rabais.Tests;
+
+public class RabaisMultipleTest
+{
+    public static IList<object[]> StrategieRabaisNullData => new List<object[]>
+    {
+        new object[] { new RabaisNul(), null },
+        new object[] { null, new RabaisNul() },
+    };
+
+
+    [Theory]
+    [MemberData(nameof(StrategieRabaisNullData))]
+    public void Constructor_StrategieRabaisNull_LanceErreur(IStrategieRabais rabais1, IStrategieRabais rabais2)
+    {
+        Assert.Throws<ArgumentNullException>(() =>
+            new RabaisMultiple(rabais1, rabais2)
+        );
+    }
+}
---
+++ S05E04_Restaurant_Rabais/RabaisMultiple.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant.Rabais;
+
+public class RabaisMultiple : IStrategieRabais
+{
+    private IStrategieRabais rabais1;
+    private IStrategieRabais rabais2;
+
+
+    public RabaisMultiple(IStrategieRabais rabais1, IStrategieRabais rabais2)
+    {
+        ArgumentNullException.ThrowIfNull(rabais1);
+        this.rabais1 = rabais1;
+
+        ArgumentNullException.ThrowIfNull(rabais2);
+        this.rabais2 = rabais2;
+    }
+
+    public decimal CalculerRabais(decimal sousTotal)
+    {
+        decimal total = this.rabais1.CalculerRabais(sousTotal);
+        total = this.rabais2.CalculerRabais(total);
+        return ReglesRabais.LimiterTotalAZero(total);
+    }
+}
