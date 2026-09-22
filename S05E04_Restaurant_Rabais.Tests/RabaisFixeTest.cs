8427222e864243df8890623ce08e9f84464785e6
abernatchez <abernatchez@tutamail.com>
2026-09-22 17:58:34 -0400

    Ajoute les tests pour le calcul de rabais

---
+++ S05E04_Restaurant_Rabais.Tests/RabaisFixeTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant.Rabais.Tests;
+
+public class RabaisFixeTest
+{
+    [Theory]
+    [InlineData(10, 5)]
+    [InlineData(5, 0)]
+    [InlineData(3, 0)]
+    public void CalculerRabais_Theory_EstEgaleAuRésultatAttendu(decimal sousTotal, decimal expected)
+    {
+        RabaisFixe rabaisFixe = new();
+
+        decimal result = rabaisFixe.CalculerRabais(sousTotal);
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ S05E04_Restaurant_Rabais.Tests/RegleRabaisTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant.Rabais.Tests;
+
+public class RegleRabaisTest
+{
+    [Theory]
+    [InlineData(-16, 0)]
+    [InlineData(0, 0)]
+    [InlineData(5, 5)]
+    public void LimiterTotalAZero_Theory_EstEgaleAuRésultatAttendu(decimal total, decimal expected)
+    {
+        decimal result = ReglesRabais.LimiterTotalAZero(total);
+
+        Assert.Equal(expected, result);
+    }
+}
